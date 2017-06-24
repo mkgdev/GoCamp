b0e5b2a74fc533a44e32095277272cdf503ade20
mkgdev <mukeshinhere07@gmail.com>
2017-06-24 11:09:20 +0530

    Listening Route Fixed for heroku

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ app.use(IndexRoutes);
 //=======================================================
 //             Listening Port
 //========================================================
-app.listen(2000, function(){
+app.listen(process.env.PORT,process.env.IP, function(){
     
     
    console.log("Yelpcamp server has started");
